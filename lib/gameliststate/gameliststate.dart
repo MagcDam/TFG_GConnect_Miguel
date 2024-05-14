c54936c1e56ae38b905b1578d156ee6788f22bc7
MagcDam <a_miguelangel.gutierrez.calderon@iespablopicasso.es>
2024-05-14 17:10:06 +0200

    diseño pagina principal

--- lib/gameliststate/gameliststate.dart
+++ lib/gameliststate/gameliststate.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:gconnect/gamedetailpage/gamedetailpage.dart';
+import 'package:gconnect/creditspage/creditspage.dart';
+import 'package:gconnect/gamesearchpage/gamesearchpage.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class GameList extends StatefulWidget {
-  const GameList({super.key}); // Constructor de la clase GameList
+  const GameList({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
-  _GameListState createState() => _GameListState(); // Método que devuelve una instancia de _GameListState
+  _GameListState createState() => _GameListState();
 }
 
 class _GameListState extends State<GameList> {
-  final int pageSize = 50; // Tamaño de página para la paginación
-  int currentPage = 1; // Número de página actual
-  List<dynamic> games = []; // Lista para almacenar los juegos
+  final int pageSize = 39;
+  int currentPage = 1;
+  List<dynamic> games = [];
 
-  // Método para obtener los juegos desde la API
   Future<void> fetchGames() async {
     final Uri uri = Uri.parse(
         'https://api.rawg.io/api/games?key=8b39d93d87ba4d90bc0a1db9c0aea2b9&page_size=$pageSize&page=$currentPage');
@@ -24,51 +24,140 @@ class _GameListState extends State<GameList> {
     final response = await http.get(uri);
     if (response.statusCode == 200) {
       setState(() {
-        games.addAll(json.decode(response.body)['results']); // Agrega los juegos obtenidos a la lista de juegos
-        currentPage++; // Incrementa el número de página para la próxima solicitud
+        games.addAll(json.decode(response.body)['results']);
+        currentPage++;
       });
     } else {
-      throw Exception('Failed to load the games'); // Lanza una excepción si falla la carga de juegos
+      throw Exception('Failed to load the games');
     }
   }
 
   @override
   void initState() {
     super.initState();
-    fetchGames(); // Llama al método para obtener los juegos al inicializar el estado
+    fetchGames();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Most popular games'), // Título de la barra de aplicación
+        title: const Text('Most popular games',
+        style: TextStyle(color: Colors.red),),
+        backgroundColor: const Color.fromARGB(255, 39, 39, 39),
       ),
-      body: ListView.builder(
-        itemCount: games.length + 1, // Establece la cantidad de elementos en la lista, más uno para el botón de carga
-        itemBuilder: (context, index) {
-          if (index == games.length) { // Si el índice es igual a la longitud de la lista de juegos, muestra el botón de carga
-            return ElevatedButton(
-              onPressed: fetchGames, // Llama al método para obtener más juegos al presionar el botón
-              child: const Text('Load more games'), // Texto del botón de carga
-            );
-          }
-          return InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => GameDetailPage(
-                    gameDetails: games[index], // Pasa los detalles del juego seleccionado a la página de detalles del juego
-                  ),
+      drawer: Drawer(
+        child: Container(
+          color: const Color.fromARGB(255, 39, 39, 39),
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 39, 39, 39),
                 ),
-              );
-            },
-            child: ListTile(
-              title: Text(games[index]['name']), // Muestra el nombre del juego en la lista
+                child: Text(
+                  'GConnect Menu',
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  'Search',
+                  style: TextStyle(color: Colors.red),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => GameSearchPage()),
+                  );
+                },
+              ),
+              ListTile(
+                title: Text(
+                  'Credits',
+                  style: TextStyle(color: Colors.red),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => creditspage()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Container(
+        color: const Color.fromARGB(255, 39, 39, 39),
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                itemCount: games.length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => GameDetailPage(
+                            gameDetails: games[index],
+                          ),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.red),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: Image.network(
+                              games[index]['background_image'],
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            games[index]['name'],
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
-          );
-        },
+            SizedBox(height: 20), // Espacio entre la lista de juegos y el botón
+            Material(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: ElevatedButton(
+                onPressed: fetchGames,
+                child: Text(
+                  'Load more games',
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
